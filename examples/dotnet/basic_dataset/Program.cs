fdc6d2b5b3a14a445235eaa76ac598e73f4c7e69
Martin Thwaites <github@my2cents.co.uk>
2023-11-01 01:10:49 +0000

    added .NET example

---
+++ examples/dotnet/basic_dataset/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Honeycomb.Resources;
+
+return await Deployment.RunAsync(() =>
+{
+   // Add your resources here
+
+   var dataset = new Dataset("my-pulumi-dataset", new DatasetArgs {
+      Name = "my-pulumi-dataset",
+      Description = "Created from pulumi",
+      Expand_json_depth = 3
+   });
+
+   // Export outputs here
+   return new Dictionary<string, object?>
+   {
+      ["outputKey"] = "outputValue"
+   };
+});
