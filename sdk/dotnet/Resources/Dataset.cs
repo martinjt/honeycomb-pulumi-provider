4866f4851a7e9b41ccda1352cd645e409bf64424
MartinDotNet <github@my2cents.co.uk>
2023-11-02 12:49:27 +0000

    Added plugin url

--- sdk/dotnet/Resources/Dataset.cs
+++ sdk/dotnet/Resources/Dataset.cs
@@ -50,6 +50,7 @@ namespace Pulumi.Honeycomb.Resources
             var defaultOptions = new CustomResourceOptions
             {
                 Version = Utilities.Version,
+                PluginDownloadURL = "github://api.github.com/martinjt/honeycomb-pulumi-provider",
             };
             var merged = CustomResourceOptions.Merge(defaultOptions, options);
             // Override the ID if one was specified for consistency with other language SDKs.
--- sdk/dotnet/Utilities.cs
+++ sdk/dotnet/Utilities.cs
@@ -53,6 +53,7 @@ namespace Pulumi.Honeycomb
         {
             var dst = src ?? new global::Pulumi.InvokeOptions{};
             dst.Version = src?.Version ?? Version;
+            dst.PluginDownloadURL = src?.PluginDownloadURL ?? "github://api.github.com/martinjt/honeycomb-pulumi-provider";
             return dst;
         }
 
